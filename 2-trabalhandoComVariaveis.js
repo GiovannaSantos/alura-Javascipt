5ef592749c0ad0d3888ca799918518ae0e855017
Giovanna Santos <contatogiovannasantos@outlook.com.br>
2021-06-10 19:52:20 -0300

    Curso JavaScript Alura - JavaScript: primeiros passos com a linguagem

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 22;
+const nome = "Giovanna";
+
+//não fazer
+//mes = "Janeiro";
